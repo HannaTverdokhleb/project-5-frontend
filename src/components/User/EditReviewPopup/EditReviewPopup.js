8f11526328295888ab725aad2abe58354bca7703
HannaTverdokhleb <1annakiss92@gmail.com>
2023-09-27 23:57:03 +0400

    structure

---
+++ src/components/User/EditReviewPopup/EditReviewPopup.js
@@ -0,0 +1,6 @@
+// const EditReviewPopup = () => {
+//     return ReactDOM.createPortal(
+//       <div>PopupWindow with portal</div>,
+//       document.querySelector("#popup-root")
+//     );
+//   };
