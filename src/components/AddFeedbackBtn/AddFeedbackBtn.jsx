b91b5331e2775be54f8e228ed0776b336a70f31d
Andrii Stepanenko <andrii.stp@gmail.com>
2023-09-28 13:22:22 +0300

    full button with css/ need props from Header

--- src/components/AddFeedbackBtn/AddFeedbackBtn.jsx
+++ src/components/AddFeedbackBtn/AddFeedbackBtn.jsx
@@ -1,7 +1,14 @@
+//Готова кнопка зі стилями
+//приймає пропс setOpen з Хедера, при кліку має відкрити модалку, Треба вписати пропс
+
+import css from './AddFeebbackBtn.module.css';
+
 const AddFeedbackBtn = () => {
   return (
-    <button type="button" onClick={() => {}}>
+    <button className={css.feedbackBtn} type="button" onClick={() => {}}>
       Feedback
     </button>
   );
 };
+
+export default AddFeedbackBtn;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
+import AddFeedbackBtn from './AddFeedbackBtn/AddFeedbackBtn';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <AddFeedbackBtn />;
 };
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
