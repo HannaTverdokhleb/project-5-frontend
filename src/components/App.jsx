f882fb163adac04336e2a8a8eee65aa1e06786ac
HannaTverdokhleb <1annakiss92@gmail.com>
2023-09-29 17:34:04 +0400

    default pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 const Homepage = lazy(() => import('../pages/Homepage'));
 const LoginPage = lazy(() => import('../pages/Login'));
@@ -11,14 +11,16 @@ const Page404 = lazy(() => import('../pages/404'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/account" element={<AccountPage />} />
-      <Route path="/calendar" element={<CalendarPage />} />
-      <Route path="/statistics" element={<StatisticsPage />} />
-      <Route path="*" element={<Page404 />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/statistics" element={<StatisticsPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </Suspense>
   );
 };
