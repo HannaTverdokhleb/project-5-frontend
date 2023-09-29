2eacb12f677c24ac3ce8381f5e9de9c40be49cff
HannaTverdokhleb <1annakiss92@gmail.com>
2023-09-29 16:16:15 +0400

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { lazy } from "react";
+import { Route, Routes } from 'react-router-dom';
+const HomePage = lazy(() => import('../pages/Homepage'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const Account = lazy(() => import('../pages/AccountPage'));
+const Calendar = lazy(() => import('../pages/CalendarPage'));
+const Statistics = lazy(() => import('../pages/StatisticsPage'));
+const Page404 = lazy(() => import('../pages/404'));
+
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/calendar" element={<Calendar />} />
+      <Route path="/statistics" element={<Statistics />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
 };
