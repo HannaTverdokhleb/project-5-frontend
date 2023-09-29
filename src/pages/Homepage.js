8dd6c8edba07a2e57503bb71d3371001dd449ff8
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-09-29 20:39:36 +0300

    added reviews section

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,14 @@
 import { Description } from 'components/Homepage/DescSection/DescSection';
 import { HeroSection } from 'components/Homepage/HeroSection/HeroSection';
+import { ReviewsSlider } from 'components/Homepage/ReviewsSection/ReviewsSection';
+
 
 const Homepage = () => {
   return (
     <>
       <HeroSection />
       <Description />
+      <ReviewsSlider />
     </>
   );
 };
