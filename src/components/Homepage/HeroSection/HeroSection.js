1dee35daff225a0106c73e08c0b7f5578502f04f
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-09-29 20:09:56 +0300

    icon fixed

--- src/components/Homepage/HeroSection/HeroSection.js
+++ src/components/Homepage/HeroSection/HeroSection.js
@@ -7,7 +7,6 @@ import gooseImageTablet from 'images/tabletImages/heroSection/heroGoose_tab@1x.p
 import gooseImageTablet2x from 'images/tabletImages/heroSection/heroGoose_tab@2x.png';
 import gooseImageMobile from 'images/mobileImages/heroSection/heroGoose_mob@1x.png';
 import gooseImageMobile2x from 'images/mobileImages/heroSection/heroGoose_mob@2x.png';
-import arrowIcon from 'images/sprite.svg';
 
 export const HeroSection = () => {
   return (
@@ -42,16 +41,28 @@ export const HeroSection = () => {
           G<span className={css.titleItalic}>oo</span>seTrack
         </h1>
         <nav className={css.contentButtons}>
-      <Link to="/login" className={css.buttonWhite}>
-        Log in
-        <svg className={css.arrowIcon} width="13.5" height="13.5">
-        <use xlinkHref={`${arrowIcon}#icon-arrow-right-link`} />
-        </svg>
-      </Link>
-      <Link to="/register" className={css.textWhite}>
-        Sign Up
-      </Link>
-    </nav>
+          <Link to="/login" className={css.buttonWhite}>
+            Log in
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="19"
+              height="18"
+              viewBox="0 0 19 18"
+              fill="none"
+            >
+              <path
+                d="M11.75 2.25006H12.65C13.9101 2.25006 14.5402 2.25006 15.0215 2.4953C15.4448 2.71101 15.789 3.05522 16.0048 3.47858C16.25 3.95988 16.25 4.58995 16.25 5.85006V12.1501C16.25 13.4102 16.25 14.0402 16.0048 14.5215C15.789 14.9449 15.4448 15.2891 15.0215 15.5048C14.5402 15.7501 13.9101 15.7501 12.65 15.7501H11.75M8 5.25006L11.75 9.00006M11.75 9.00006L8 12.7501M11.75 9.00006L2.75 9.00006"
+                stroke="#3E85F3"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </Link>
+          <Link to="/register" className={css.textWhite}>
+            Sign Up
+          </Link>
+        </nav>
       </div>
     </div>
   );
