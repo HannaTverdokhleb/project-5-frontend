76b9a131b4eb9f7fe9d5e669441a954e2c6dae65
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-09-30 17:27:38 +0300

    added gradients

--- src/components/User/UserStatistics/UserStatistics.js
+++ src/components/User/UserStatistics/UserStatistics.js
@@ -4,11 +4,11 @@ import {
   Bar,
   XAxis,
   YAxis,
-//   Tooltip,
-//   Legend,
+  //   Tooltip,
+  //   Legend,
   CartesianGrid,
 } from 'recharts';
-import css from './UserStatistics.module.css'
+import css from './UserStatistics.module.css';
 
 const data = [
   { name: 'To Do', month: 100, day: 60, amt: 100 },
@@ -16,54 +16,71 @@ const data = [
   { name: 'Done', month: 100, day: 80, amt: 100 },
 ];
 
+const gradientPink = (
+  <linearGradient id="barGradientPink" x1="0%" y1="0%" x2="0%" y2="100%">
+    <stop offset="0%" stopColor="#ffffff" />
+    <stop offset="100%" stopColor="#FFD2DD" />
+  </linearGradient>
+);
+
+const gradientBlue = (
+    <linearGradient id="barGradientBlue" x1="0%" y1="0%" x2="0%" y2="100%">
+      <stop offset="0%" stopColor="#ffffff" />
+      <stop offset="100%" stopColor="#3E85F3" />
+    </linearGradient>
+  );
+
 //rounded bar
-const RoundedBar = (props) => {
-    const { x, y, width, height, fill } = props;
-    const borderRadius = 6; // Adjust the border radius as needed
-  
-    return (
-      <g>
-        <rect
-          x={x}
-          y={y}
-          width={width}
-          height={height}
-          fill={fill}
-          rx={borderRadius}
-          ry={borderRadius}
-        />
-      </g>
-    );
-  };
+const RoundedBar = props => {
+  const { x, y, width, height, fill } = props;
+  const borderRadius = 6;
+
+  return (
+    <g>
+      <rect
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        fill={fill}
+        rx={borderRadius}
+        ry={borderRadius}
+      />
+    </g>
+  );
+};
 
 export const StatisticsChart = () => {
   return (
     <div className={css.container}>
-    <BarChart
-      width={320}
-      height={266}
-      data={data}
-      //   margin={{
-      //     top: 5,
-      //     right: 30,
-      //     left: 20,
-      //     bottom: 5,
-      //   }}
-      barSize={22}
-      barGap={8}
-      barCategoryGap="30px"
-    >
-      <CartesianGrid stroke="#E3F3FF" />
-      <CartesianGrid vertical={false} />
-      {/* <CartesianGrid strokeDasharray="3 3" /> */}
+      <BarChart
+        width={320}
+        height={266}
+        data={data}
+        //   margin={{
+        //     top: 5,
+        //     right: 30,
+        //     left: 20,
+        //     bottom: 5,
+        //   }}
 
-      <XAxis dataKey="name" />
-      <YAxis />
-      {/* <Tooltip /> */}
-      {/* <Legend /> */}
-      <Bar dataKey="day" fill="#FFD2DD" shape={<RoundedBar />}/>
-      <Bar dataKey="month" fill="#3E85F3" shape={<RoundedBar />}/>
-    </BarChart>
+        barSize={22}
+        barGap={8}
+        barCategoryGap="30px"
+      >
+        <defs>{gradientPink}</defs>
+        <defs>{gradientBlue}</defs>
+        <CartesianGrid stroke="#E3F3FF" />
+        <CartesianGrid vertical={false} />
+        {/* <CartesianGrid strokeDasharray="3 3" /> */}
+
+        <XAxis dataKey="name" />
+        <YAxis />
+        {/* <Tooltip /> */}
+        {/* <Legend /> */}
+        <Bar dataKey="day" fill="url(#barGradientPink)" shape={<RoundedBar />} />
+        <Bar dataKey="month" fill="url(#barGradientBlue)" shape={<RoundedBar />} />
+      </BarChart>
     </div>
   );
 };
