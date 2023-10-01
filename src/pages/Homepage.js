a58ce46e0b4b686c128fde427010f7a32ce16a0a
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-10-01 17:18:53 +0300

    removed new components from home page preview

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,14 +1,10 @@
 import { Description } from 'components/Homepage/DescSection/DescSection';
 import { HeroSection } from 'components/Homepage/HeroSection/HeroSection';
 import { ReviewsSlider, TitleH2 } from 'components/Homepage/ReviewsSection/ReviewsSection';
-import { CalendarDropdown } from 'components/User/CalendarPopup/CalendarPopup';
-import { StatisticsChart } from 'components/User/UserStatistics/UserStatistics';
 
 const Homepage = () => {
   return (
     <>
-    <CalendarDropdown/>
-    <StatisticsChart/>
       <HeroSection />
       <Description />
       <TitleH2/>
