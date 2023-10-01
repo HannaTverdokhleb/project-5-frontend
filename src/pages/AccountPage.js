ceeb30a3d030bb6be5e9f44c79cd672aa062ca94
Symeon Melnychuk <m.simeon998@gmail.com>
2023-10-01 19:06:27 +0300

    add media

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,6 +1,11 @@
 import { Header } from 'components/User/Header/Header';
 
 const AccountPage = () => {
+<<<<<<< Updated upstream
+=======
+  const dispatch = useDispatch();
+  dispatch(currentPage('User Profile'));
+>>>>>>> Stashed changes
   return (
     <div>
       Account
