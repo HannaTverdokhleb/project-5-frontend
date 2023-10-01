97b7255de1377d83e29fcf1fcec2e935d3695a48
HannaTverdokhleb <1annakiss92@gmail.com>
2023-10-01 21:34:07 +0400

    fix

--- src/pages/CalendarPage/CalendarPage.js
+++ src/pages/CalendarPage/CalendarPage.js
@@ -1,18 +1,19 @@
-import Header from 'components/User/Header/Header';
-import MainLayout from 'components/User/MainLayout/MainLayout';
-import Sidebar from 'components/User/Sidebar/Sidebar';
-import UserCalendar from 'components/User/UserCalendar/UserCalendar';
-import style from './CalendarPage.module.css';
+// import Header from 'components/User/Header/Header';
+// import MainLayout from 'components/User/MainLayout/MainLayout';
+// import Sidebar from 'components/User/Sidebar/Sidebar';
+// import UserCalendar from 'components/User/UserCalendar/UserCalendar';
+// import style from './CalendarPage.module.css';
 
 const CalendarPage = () => {
   return (
-    <MainLayout>
-      <Sidebar />
-      <div className={style.innerWrapper}>
-        <Header />
-        <UserCalendar />
-      </div>
-    </MainLayout>
+    // <MainLayout>
+    //   <Sidebar />
+    //   <div className={style.innerWrapper}>
+    //     <Header />
+    //     <UserCalendar />
+    //   </div>
+    // </MainLayout>
+    <div>Calendar</div>
   );
 };
 
