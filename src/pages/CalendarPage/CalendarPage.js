eddeb44fb543acb9fb1802f43be9885ed0d3e42d
Symeon Melnychuk <m.simeon998@gmail.com>
2023-10-01 21:23:52 +0300

    2

--- src/pages/CalendarPage/CalendarPage.js
+++ src/pages/CalendarPage/CalendarPage.js
@@ -1,22 +1,25 @@
-// import Header from 'components/User/Header/Header';
 // import MainLayout from 'components/User/MainLayout/MainLayout';
 // import Sidebar from 'components/User/Sidebar/Sidebar';
 // import UserCalendar from 'components/User/UserCalendar/UserCalendar';
 // import style from './CalendarPage.module.css';
+import { Header } from 'components/User/Header/Header';
 import { useDispatch } from 'react-redux';
 import { currentPage } from 'redux/actions';
 const CalendarPage = () => {
   const dispatch = useDispatch();
-  dispatch(currentPage('User Profile'));
+  dispatch(currentPage('Calendar'));
   return (
     // <MainLayout>
     //   <Sidebar />
     //   <div className={style.innerWrapper}>
-    //     <Header />
+    //
     //     <UserCalendar />
     //   </div>
     // </MainLayout>
-    <div>Calendar</div>
+    <div>
+      <Header />
+      Calendar
+    </div>
   );
 };
 
