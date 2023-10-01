c868ea181cb220b7cd433168045416bec0d1e202
Symeon Melnychuk <m.simeon998@gmail.com>
2023-10-01 19:10:26 +0300

    add media

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,9 @@
+<<<<<<< Updated upstream
 import { namePageReducer } from './reducer';
 
+=======
+<<<<<<< HEAD
+>>>>>>> Stashed changes
 const { configureStore } = require('@reduxjs/toolkit');
 
 export const store = configureStore({
@@ -7,3 +11,43 @@ export const store = configureStore({
     currentPage: namePageReducer,
   },
 });
+=======
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { authReducer } from './Auth/AuthSlice';
+import { reviewsReducer } from './reviews/reviewsSlice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    reviews: reviewsReducer,
+    // tasks: tasksReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export const persistor = persistStore(store);
+>>>>>>> 1eafb27311cf4d1be0b30807191dac703054d964
