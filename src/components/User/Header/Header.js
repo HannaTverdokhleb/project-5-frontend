e7eecf295669cff0f19366f09600c84fe9b86201
Symeon Melnychuk <m.simeon998@gmail.com>
2023-10-02 20:43:32 +0300

    add burger

--- src/components/User/Header/Header.js
+++ src/components/User/Header/Header.js
@@ -1,10 +1,19 @@
 import { useSelector } from 'react-redux';
 import css from './Header.module.css';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 export const Header = () => {
   const namePage = useSelector(state => state.currentPage.namePage);
+
   return (
     <header>
+      <button
+        type="button"
+        className={css.burgerButton}
+        aria-label="open close burger menu"
+      >
+        <RxHamburgerMenu className={css.burgerIcon} />
+      </button>
       <section>{<h1 className={css.title}>{namePage}</h1>}</section>
     </header>
   );
