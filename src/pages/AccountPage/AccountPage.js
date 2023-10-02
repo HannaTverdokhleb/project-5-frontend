898e69f4b71de823159a96dd57e92b6cd045fc45
Andrii Stepanenko <andrii.stp@gmail.com>
2023-10-02 17:37:34 +0300

    init

--- src/pages/AccountPage/AccountPage.js
+++ src/pages/AccountPage/AccountPage.js
@@ -1,6 +1,7 @@
 import { Header } from 'components/User/Header/Header';
 import { useDispatch } from 'react-redux';
 import { currentPage } from 'redux/actions';
+
 const AccountPage = () => {
   const dispatch = useDispatch();
   dispatch(currentPage('User Profile'));
