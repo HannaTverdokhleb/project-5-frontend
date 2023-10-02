5ed47e477f59e0a9f56764cd650125f93569c2dd
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-10-02 13:55:31 +0300

    add ThemeToggler in Header

--- src/components/User/Header/Header.js
+++ src/components/User/Header/Header.js
@@ -1,11 +1,15 @@
 import { useSelector } from 'react-redux';
 import css from './Header.module.css';
+import ThemeToggler from '../ThemeToggler/ThemeToggler';
 
 export const Header = () => {
   const namePage = useSelector(state => state.currentPage.namePage);
   return (
     <header>
       <section>{<h1 className={css.title}>{namePage}</h1>}</section>
+      <section>
+        <ThemeToggler></ThemeToggler>
+      </section>
     </header>
   );
 };
