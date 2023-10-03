89154b47bd46122d0110b8345ddbde998e16ebb0
HannaTverdokhleb <1annakiss92@gmail.com>
2023-10-03 21:19:17 +0400

    fix styles in Sidebar, MainLayout styles

--- src/components/User/MainLayout/MainLayout.js
+++ src/components/User/MainLayout/MainLayout.js
@@ -1,18 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectUser } from 'redux/Auth/selectors';
 import { Header } from 'components/User/Header/Header';
+import SideBar from '../Sidebar/Sidebar';
+import css from './MainLayout.module.css';
 
 
 const MainLayout = () => {
     const user = useSelector(selectUser);
 
     return (
-        <section className="mainLayout">
-            <div className="mainLayoutContainer">
-                {/* тут буде сайдбар */}
-                <Header />
-                <Outlet context={[user]} />{/* це контент сторінки */}
+        <section className={css.mainLayout}>
+            <div className={css.mainLayoutContainer}>
+                <SideBar />
+                <div className={css.pageWrapper}>
+                    <Header />
+                    <Outlet context={[user]} />{/* це контент сторінки */}
+                </div>
             </div>
         </section>
     )
