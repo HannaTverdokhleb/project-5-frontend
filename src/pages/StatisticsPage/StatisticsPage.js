e19559ae44beb0a98cb5166da01e88c4abea3acb
Symeon Melnychuk <m.simeon998@gmail.com>
2023-10-03 08:34:05 +0300

    add dispatch in page

--- src/pages/StatisticsPage/StatisticsPage.js
+++ src/pages/StatisticsPage/StatisticsPage.js
@@ -1,15 +1,14 @@
 import { useOutletContext } from 'react-router-dom';
-
+import { useDispatch } from 'react-redux';
+import { currentPage } from 'redux/actions';
 
 const StatisticsPage = () => {
   const [user] = useOutletContext();
   console.log(user); // ТУТ ВСЯ ІНФА ПРО ЮЗЕРА, ЯКЩО ТРЕБА
+  const dispatch = useDispatch();
+  dispatch(currentPage('Statistics'));
 
-  return (
-    <div className="statisticsPage">
-      Statistics
-    </div>
-  );
+  return <div className="statisticsPage">Statistics</div>;
 };
 
 export default StatisticsPage;
