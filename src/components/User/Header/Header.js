e4d3d96dc326a1f8f3ea74478a0e0bbfd3cdf3cb
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-10-03 15:26:34 +0300

    start

--- src/components/User/Header/Header.js
+++ src/components/User/Header/Header.js
@@ -1,11 +1,15 @@
 import { useSelector } from 'react-redux';
 import css from './Header.module.css';
+import UserInfo from '../UserInfo/UserInfo';
 
 export const Header = () => {
   const namePage = useSelector(state => state.currentPage.namePage);
   return (
     <header>
       <section>{<h1 className={css.title}>{namePage}</h1>}</section>
+      <section>
+        <UserInfo></UserInfo>
+      </section>
     </header>
   );
 };
