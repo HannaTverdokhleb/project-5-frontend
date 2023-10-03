2cbfe5021e1a494391983acfbff16881d5af1d5d
Symeon Melnychuk <m.simeon998@gmail.com>
2023-10-03 16:53:05 +0300

    add goose photo in header

--- src/components/User/Header/Header.js
+++ src/components/User/Header/Header.js
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
 import css from './Header.module.css';
-
 import { RxHamburgerMenu } from 'react-icons/rx';
-
+import gooseUrl1x from 'images/desktopImages/header/header_desk@1x.png';
+import gooseUrl2x from 'images/desktopImages/header/header_desk@2x.png';
 import ThemeToggler from '../ThemeToggler/ThemeToggler';
 
 export const Header = () => {
@@ -17,7 +17,19 @@ export const Header = () => {
       >
         <RxHamburgerMenu className={css.burgerIcon} />
       </button>
-      <section>{<h1 className={css.title}>{namePage}</h1>}</section>
+      <section>
+        {<h1 className={css.title}>{namePage}</h1>}
+        {namePage === 'Calendar' && (
+          <div className={css.calendar}>
+            <img
+              srcSet={`${gooseUrl1x} 1x, ${gooseUrl2x} 2x`}
+              alt="Goose"
+              className={css.calendarGoose}
+            />
+            <p>Let go of the past and focus on the present!</p>
+          </div>
+        )}
+      </section>
       <section>
         <ThemeToggler></ThemeToggler>
       </section>
