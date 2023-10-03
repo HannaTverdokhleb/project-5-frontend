969732813f75172be38ed80936dcf4bc41e17e56
Iryna Rozmaita <iriszechka@ukr.net>
2023-10-03 16:45:39 +0300

    update route path

--- src/components/User/Sidebar/UserNav/UserNav.js
+++ src/components/User/Sidebar/UserNav/UserNav.js
@@ -11,19 +11,19 @@ function UserNav() {
         <nav>
             <ul className={css.nav_container}>
                 <li className={css.nav__item}>
-                    <NavLink className={({ isActive }) => `${css['nav__link']}  ${isActive ? css['active'] : ''}`} to="/user/account">
+                    <NavLink className={({ isActive }) => `${css['nav__link']}  ${isActive ? css['active'] : ''}`} to="/account">
                        <UserIcon className={css.nav__icon}/>
                         My account
                     </NavLink>
                 </li>
                 <li className={css.nav__item}>
-                    <NavLink className={({ isActive }) => `${css['nav__link']}  ${isActive ? css['active'] : ''}`} to="/user/calendar">
+                    <NavLink className={({ isActive }) => `${css['nav__link']}  ${isActive ? css['active'] : ''}`} to="/calendar">
                         <CalendarIcon className={css.nav__icon}/>
                         Calendar
                     </NavLink>
                 </li>
                 <li className={css.nav__item}>
-                    <NavLink className={({ isActive }) => `${css['nav__link']}  ${isActive ? css['active'] : ''}`} to="/user/statistics">
+                    <NavLink className={({ isActive }) => `${css['nav__link']}  ${isActive ? css['active'] : ''}`} to="/statistics">
                       <StatIcon className={css.nav__icon}/>
                       Statistics
                     </NavLink>
