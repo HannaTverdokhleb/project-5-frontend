a30cfdef06323b312831e0604135326999cdf272
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-10-04 23:34:51 +0300

    new height

--- src/components/User/CalendarPopup/CalendarPopup.js
+++ src/components/User/CalendarPopup/CalendarPopup.js
@@ -20,6 +20,7 @@ export const CalendarDropdown = () => {
       onChange={date => setStartDate(date)}
       customInput={<CustomInput />}
       fixedHeight={css.fixedHeight}
+     
     />
   );
 };
