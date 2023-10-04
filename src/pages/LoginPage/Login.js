3df705c9d12159b2388778249bb9d4d50b571657
HannaTverdokhleb <1annakiss92@gmail.com>
2023-10-05 00:45:12 +0400

    fix styles for login form

--- src/pages/LoginPage/Login.js
+++ src/pages/LoginPage/Login.js
@@ -1,13 +1,31 @@
 import { LoginForm } from 'components/LoginForm/LoginForm';
 import AuthNavigate from 'components/User/AuthNavigate/AuthNavigate';
+import css from './Login.module.css';
+import loginGoose from '../../images/desktopImages/loginPage/loginGoose_desk@1x.png';
+import loginGoosex2 from '../../images/desktopImages/loginPage/loginGoose_desk@2x.png';
 
 const LoginPage = () => {
 
   return (
-    <div>
-      <LoginForm />
-      <AuthNavigate authLink={'/register'} linkText={'Sign up'} />
-    </div>
+    <section className={css.form_section}>
+      <div className={css.div_container}>
+        <picture className={css.logo_goose}>
+              <source
+                className={css.source}
+                srcSet={`${loginGoose} 1x, ${loginGoosex2} 2x`}
+              />
+              <img srcSet={loginGoose} alt="rocket" />
+            </picture>
+          <div className={css.div_container_formik}>
+            <h2 className={css.title}>Login</h2>
+            <LoginForm />
+            <a className={css.google} href="https://goose-track-backend-54zr.onrender.com/auth/google">Click me to authorize with Google!</a>
+          </div>
+          <div className={css.register}>
+              <AuthNavigate authLink={'/register'} linkText={'Sign up'} />
+            </div>
+      </div>
+    </section>
   );
 };
 
