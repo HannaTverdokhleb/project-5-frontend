0189fc6b080a3c4c35625df80019ed1c0b0edf55
Symeon Melnychuk <m.simeon998@gmail.com>
2023-10-04 08:14:09 +0300

    1

--- src/components/User/MainLayout/MainLayout.js
+++ src/components/User/MainLayout/MainLayout.js
@@ -4,40 +4,22 @@ import { selectUser } from 'redux/Auth/selectors';
 import { Header } from 'components/User/Header/Header';
 import SideBar from '../Sidebar/Sidebar';
 import css from './MainLayout.module.css';
-<<<<<<< Updated upstream
 
-
-=======
->>>>>>> Stashed changes
 const MainLayout = () => {
-    const user = useSelector(selectUser);
+  const user = useSelector(selectUser);
 
-<<<<<<< Updated upstream
-    return (
-        <section className={css.mainLayout}>
-            <div className={css.mainLayoutContainer}>
-                <SideBar />
-                <div className={css.pageWrapper}>
-                    <Header />
-                    <Outlet context={[user]} />{/* це контент сторінки */}
-                </div>
-            </div>
-        </section>
-    )
-}
-=======
   return (
-    <section className="mainLayout">
+    <section className={css.mainLayout}>
       <div className={css.mainLayoutContainer}>
-        {/* тут буде сайдбар */}
         <SideBar />
-        <Header />
-        <Outlet context={[user]} />
-        {/* це контент сторінки */}
+        <div className={css.pageWrapper}>
+          <Header />
+          <Outlet context={[user]} />
+          {/* це контент сторінки */}
+        </div>
       </div>
     </section>
   );
 };
->>>>>>> Stashed changes
 
 export default MainLayout;
--- src/components/User/Sidebar/Sidebar.js
+++ src/components/User/Sidebar/Sidebar.js
@@ -15,21 +15,6 @@ export default function SideBar() {
 
   return (
     <aside className={`${css.sidebar} ${isOpen ? css.openSidebar : ''}`}>
-<<<<<<< Updated upstream
-        <button className={css.close__btn} onClick={toggleSidebar} type="button" aria-label="Close">
-          <RxCross1 className={css.close__icon} />
-        </button>
-        {/*SideBarlogo*/}
-        <div className={css.box__logosidebar}>
-          <img width="71" height="68" src={logoSidebar} alt="SidebarLogo" className={css.logo__sidebar}></img>
-          <h2 className={css.logo__title}>G</h2>
-          <span className={css.logo__change}>oo</span>
-          <h2 className={css.logo__title}>seTrack</h2>
-        </div>
-        <p className={css.aside__title}>User Panel</p>
-        <UserNav/>
-        <LogoutBtn/>
-=======
       <button
         className={css.close__btn}
         onClick={toggleSidebar}
@@ -54,7 +39,6 @@ export default function SideBar() {
       <p className={css.aside__title}>User Panel</p>
       <UserNav />
       <LogoutBtn />
->>>>>>> Stashed changes
     </aside>
   );
 }
