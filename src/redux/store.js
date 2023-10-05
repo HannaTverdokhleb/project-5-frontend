6353e351220f59fec465330ea5c75e2167a34d30
HannaTverdokhleb <1annakiss92@gmail.com>
2023-10-06 00:16:17 +0400

    Tasks Slice Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import { authReducer } from './Auth/AuthSlice';
 import { namePageReducer } from './reducer';
 import { reviewsReducer } from './reviews/reviewsSlice';
 import { userReducer } from './User/UserSlice';
+import { tasksReducer } from './Tasks/TasksSlice';
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    tasks: tasksReducer,
     reviews: reviewsReducer,
     currentPage: namePageReducer,
   },
