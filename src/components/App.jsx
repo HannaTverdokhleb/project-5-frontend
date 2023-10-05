44b7d9b1c2eb1456cc47f82142b588beda5a8d3d
Andriian Sokolenko <asokolenko@BaDM.ua>
2023-10-05 15:01:28 +0300

    test it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,72 @@
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PrivateRoute } from './PrivateRoute';
 import { RestrictedRoute } from './RestrictedRoute';
 import Loader from 'components/Loader/Loader';
 import { routes } from '../configs/routes';
+import { useDispatch } from 'react-redux';
+import { logOut, refreshUser } from '../redux/Auth/operations';
 
 const MainLayout = lazy(() => import('components/User/MainLayout/MainLayout'));
 const Page404 = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
 
-export const App = () => {
+function GlobalWrapper() {
+  const dispatch = useDispatch();
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    dispatch(refreshUser())
+      .then(() => {
+          setIsLoading(false);
+          setIsLoggedIn(true);
+        },
+      )
+      .catch(() => dispatch(logOut()));
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        {Object.values(routes.restricted).map(route =>
+    <Routes>
+      {Object.values(routes.restricted).map(route =>
+        <Route
+          key={route.key}
+          path={route.path}
+          element={
+            <RestrictedRoute
+              redirectTo={route.redirectTo}
+              component={route.component}
+            /> // /calendar/month/
+          }
+        />,
+      )}
+      {isLoggedIn && <Route path='/' element={<MainLayout />}>
+        {Object.values(routes.private).map(route =>
           <Route
             key={route.key}
             path={route.path}
             element={
-              <RestrictedRoute
+              <PrivateRoute
                 redirectTo={route.redirectTo}
                 component={route.component}
-              /> // /calendar/month/
+              />
             }
           />,
         )}
-        <Route path='/' element={<MainLayout />}>
-          {Object.values(routes.private).map(route =>
-            <Route
-              key={route.key}
-              path={route.path}
-              element={
-                <PrivateRoute
-                  redirectTo={route.redirectTo}
-                  component={route.component}
-                />
-              }
-            />,
-          )}
-        </Route>
-        <Route path='*' element={<Page404 />} />
-      </Routes>
+      </Route>}
+      <Route path='*' element={<Page404 />} />
+    </Routes>
+  );
+}
+
+export const App = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <GlobalWrapper />
     </Suspense>
   );
 };
