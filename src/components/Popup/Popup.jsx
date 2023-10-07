895008838d65600cd5fff083662b51988a5c86ef
Putinjov <putinjov@gmail.com>
2023-10-07 23:54:06 +0100

    Update Popup.jsx

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './Popup.module.css';
-import { Modal } from '../Modal/Modal';
+import { createTask, editTask, getTasks } from './redux/tasks/operations';
+import { selectTasks, selectIsLoading, selectErrorMessage } from './redux/tasks/selectors';
 import AddButton from './addButton';
 import EditButton from './editButton';
+import { Modal } from 'components/Modal/Modal';
 
-const Popup = ({ isOpen, onClose, onSubmit, task }) => {
-  const [formData, setFormData] = useState({
+const Popup = ({ isOpen, onClose, task }) => {
+  const dispatch = useDispatch();
+  const myTasks = useSelector(selectTasks);
+  const isLoadingTasks = useSelector(selectIsLoading);
+  const tasksError = useSelector(selectErrorMessage);
+
+  const initialFormData = {
     title: '',
     start: '',
     end: '',
     priority: 'low',
-  });
+  };
 
-const [selectedPriority, setSelectedPriority] = useState('');
-  
-const handleChange = (e) => {
+  const [formData, setFormData] = useState(initialFormData);
+  const [selectedPriority, setSelectedPriority] = useState('low');
+
+  const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -22,7 +31,7 @@ const handleChange = (e) => {
     }));
   };
 
-const handleTimeChange = (name) => (e) => {
+  const handleTimeChange = (name) => (e) => {
     const { value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
@@ -30,88 +39,127 @@ const handleTimeChange = (name) => (e) => {
     }));
   };
 
-const handleSubmit = () => {
-    onSubmit(formData);
-    onClose();
+  const handleSubmit = async () => {
+    try {
+      if (task) {
+        await dispatch(editTask({ _id: task._id, ...formData }));
+      } else {
+        await dispatch(createTask(formData));
+      }
+      onClose();
+      dispatch(getTasks());
+    } catch (error) {
+      console.error('saving error:', error);
+    }
   };
 
-const priorities = ['Low', 'Medium', 'High'];
-    
-const handlePriorityChange = (event, priority) => {
+  const priorities = ['Low', 'Medium', 'High'];
+
+  const handlePriorityChange = (priority) => {
     setSelectedPriority(priority.toLowerCase());
+    setFormData((prevData) => ({
+      ...prevData,
+      priority: priority.toLowerCase(),
+    }));
   };
 
-    return (
-      <Modal>
-    <div className={`${styles.popupContainer} ${isOpen ? styles.open : ''}`}>
-      <div className={styles.popup}>
-        <div className={styles.closeButton} onClick={onClose}>
+  useEffect(() => {
+    dispatch(getTasks());
+  }, [dispatch]);
 
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="title" className={styles.label}>Title</label>
+  useEffect(() => {
+    if (task) {
+      setFormData(task);
+      setSelectedPriority(task.priority.toLowerCase());
+    } else {
+      setFormData(initialFormData);
+      setSelectedPriority('low');
+    }
+  }, [task]);
+
+  return (
+    <Modal>
+      <div className={`${styles.popupContainer} ${isOpen ? styles.open : ''}`}>
+        <div className={styles.popup}>
+          <div className={styles.closeButton} onClick={onClose}></div>
+          <div className={styles.formGroup}>
+            <label htmlFor="title" className={styles.label}>
+              Title
+            </label>
             <input
-                className={styles.inputTitle}
-            type="text"
-            name="title"
-            id="title"
-            value={formData.title}
-            onChange={handleChange}
-            placeholder="Enter text"
-          />
-        </div>
-        <div className={styles.timeInputsContainer}>
-          <div className={styles.timeGroup}>
-            <label htmlFor="start" className={styles.label}>Start</label>
-            <input
-              type="time"
-              name="start"
-              id="start"
-              value={formData.start}
-              onChange={handleTimeChange('start')}
-              className={styles.timeInput}
+              className={styles.inputTitle}
+              type="text"
+              name="title"
+              id="title"
+              value={formData.title}
+              onChange={handleChange}
+              placeholder="Enter text"
             />
           </div>
-          <div className={styles.timeGroup}>
-            <label htmlFor="end" className={styles.label}>End</label>
-            <input
-              type="time"
-              name="end"
-              id="end"
-              value={formData.end}
-              onChange={handleTimeChange('end')}
-              className={styles.timeInput}
-            />
+          <div className={styles.timeInputsContainer}>
+            <div className={styles.timeGroup}>
+              <label htmlFor="start" className={styles.label}>
+                Start
+              </label>
+              <input
+                type="time"
+                name="start"
+                id="start"
+                value={formData.start}
+                onChange={handleTimeChange('start')}
+                className={styles.timeInput}
+              />
+            </div>
+            <div className={styles.timeGroup}>
+              <label htmlFor="end" className={styles.label}>
+                End
+              </label>
+              <input
+                type="time"
+                name="end"
+                id="end"
+                value={formData.end}
+                onChange={handleTimeChange('end')}
+                className={styles.timeInput}
+              />
+            </div>
           </div>
-        </div>
-        <div className={styles.radioContainer}>
-          <div className={styles.radioGroup}>
-            {priorities.map((priority) => (
+          <div className={styles.radioContainer}>
+            <div className={styles.radioGroup}>
+              {priorities.map((priority) => (
                 <label
-                className={`${styles.radioLabel} ${selectedPriority === priority.toLowerCase() ? styles.checked : ''}`}
-          onClick={(event) => handlePriorityChange(event, priority)}
+                  className={`${styles.radioLabel} ${
+                    selectedPriority === priority.toLowerCase()
+                      ? styles.checked
+                      : ''
+                  }`}
+                  onClick={() => handlePriorityChange(priority)}
+                  key={priority}
                 >
-                <span className={`${styles.customIcon} ${styles[`customIcon${priority}`]}`}></span>
-                <input
-                  type="radio"
-                  name="priority"
-                  value={priority.toLowerCase()}
-                  checked={formData.priority === priority.toLowerCase()}
+                  <span
+                    className={`${styles.customIcon} ${styles[`customIcon${priority}`]}`}
+                  ></span>
+                  <input
+                    type="radio"
+                    name="priority"
+                    value={priority.toLowerCase()}
+                    checked={formData.priority === priority.toLowerCase()}
                     onChange={handleChange}
                     className={styles.radioIcon}
-                />
-                    {priority}
-              </label>
-            ))}
+                  />
+                  {priority}
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className={styles.buttonGroup}>
+            {task ? <EditButton onClick={handleSubmit} /> : <AddButton onClick={handleSubmit} />}
+            <button className={styles.cancelButton} onClick={onClose}>
+              Cancel
+            </button>
           </div>
         </div>
-        <div className={styles.buttonGroup}>
-                        {task ? (< EditButton onClick={handleSubmit} />)
-                            : (< AddButton onClick={handleSubmit} />)}
-            <button className={styles.cancelButton} onClick={onClose}>Cancel</button>
-        </div>
       </div>
-            </div>
     </Modal>
   );
 };
