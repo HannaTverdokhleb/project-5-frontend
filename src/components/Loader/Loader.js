9e76466e473b22b63410f6f86fe71e4b4bf697bd
HannaTverdokhleb <1annakiss92@gmail.com>
2023-10-09 01:03:43 +0400

    userForm

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,9 +1,15 @@
 import css from './Loader.module.css';
 
 
-const Loader = () => {
+const Loader = ({ absolute }) => {
     return (
+        <>
+        {absolute ?
+        <div className={css.absolute}><div className={css.loader}><i></i></div></div>
+        :
         <div className={css.loader}><i></i></div>
+        }
+        </>
     )
 }
 
