8fc73374c4075bc9976852311325f7d663e18441
Andriian Sokolenko <asokolenko@BaDM.ua>
2023-10-10 12:21:33 +0300

    fix sidebar behaivor

--- src/components/User/Sidebar/Sidebar.js
+++ src/components/User/Sidebar/Sidebar.js
@@ -4,25 +4,25 @@ import { RxCross1 } from 'react-icons/rx';
 import logoSidebar from 'images/logoSidebar.png';
 import css from './Sidebar.module.css';
 
-export default function SideBar({toggleSidebar, isOpenSidebar}) {
+export default function SideBar({ toggleSidebar, isOpenSidebar }) {
 
   return (
     <aside className={`${css.sidebar} ${isOpenSidebar ? css.openSidebar : ''}`}>
       <button
         className={css.close__btn}
         onClick={toggleSidebar}
-        type="button"
-        aria-label="Close"
+        type='button'
+        aria-label='Close'
       >
         <RxCross1 className={css.close__icon} />
       </button>
       {/*SideBarlogo*/}
       <div className={css.box__logosidebar}>
         <img
-          width="71"
-          height="68"
+          width='71'
+          height='68'
           src={logoSidebar}
-          alt="SidebarLogo"
+          alt='SidebarLogo'
           className={css.logo__sidebar}
         ></img>
         <h2 className={css.logo__title}>G</h2>
@@ -30,7 +30,7 @@ export default function SideBar({toggleSidebar, isOpenSidebar}) {
         <h2 className={css.logo__title}>seTrack</h2>
       </div>
       <p className={css.aside__title}>User Panel</p>
-      <UserNav />
+      <UserNav toggleSidebar={toggleSidebar} />
       <LogoutBtn />
     </aside>
   );
