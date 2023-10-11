68643e9acb8652c232767bfad9a9903ea4d19d0f
HannaTverdokhleb <1annakiss92@gmail.com>
2023-10-12 00:02:33 +0400

    PeriodTypeSelect

--- src/components/User/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.js
+++ src/components/User/CalendarToolbar/PeriodTypeSelect/PeriodTypeSelect.js
@@ -1,21 +1,29 @@
-import { NavLink } from 'react-router-dom';
-import css from './PeriodTypeSelect.module.css'
+import { NavLink, useParams } from 'react-router-dom';
+import moment from 'moment';
+import css from './PeriodTypeSelect.module.css';
 
 export default function PeriodTypeSelect() {
-    return (
-         <nav className={css.navigation}>
-      <NavLink
-        to="calendar/month"
-                className={({ isActive }) => `${css['periodBtnLeft']}  ${isActive ? css['active'] : ''}`} 
-      >
-        Month
-      </NavLink>
-      <NavLink
-        to="calendar/day"
+  const { day, month } = useParams();
+  const now = moment();
+  let date;
+  if (month) {
+    if (month === now.format('YYYY-MM')) {
+      date = moment(month + '-' + now.format('DD'), 'YYYY-MM-DD');
+    } else {
+      date = moment(month + '-01', 'YYYY-MM-DD');
+    }
+  } else {
+    date = moment(day, 'YYYY-MM-DD');
+  }
+
+  return (
+    <nav className={css.navigation}>
+      <NavLink to={'/calendar/month/' + date.format('YYYY-MM')}
+        className={({ isActive }) => `${css['periodBtnLeft']}  ${isActive ? css['active'] : ''}`} 
+      >Month</NavLink>
+      <NavLink to={'/calendar/day/' + date.format('YYYY-MM-DD')}
         className={({ isActive }) => `${css['periodBtnRight']}  ${isActive ? css['active'] : ''}`} 
-      >
-      Day  
-      </NavLink>
+      >Day </NavLink>
     </nav>
   ); 
 }
