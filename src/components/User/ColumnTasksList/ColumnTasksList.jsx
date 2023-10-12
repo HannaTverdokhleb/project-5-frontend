f568eef005133a09e36f6203c2fe5cf756254329
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-10-12 16:15:09 +0300

    Add sorting tasks by updatedAt

--- src/components/User/ColumnTasksList/ColumnTasksList.jsx
+++ src/components/User/ColumnTasksList/ColumnTasksList.jsx
@@ -2,19 +2,33 @@ import { selectUser } from 'redux/Auth/selectors';
 import TaskColumnCard from '../TaskColumnCard/TaskColumnCard';
 import css from './ColumnTasksList.module.css';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 const ColumnTasksList = ({ tasks, openPopup, handleTask }) => {
   const user = useSelector(selectUser);
+
+  const [sortedTasks, setSortedTasks] = useState([]);
+
+  useEffect(() => {
+    const sortedTasks = tasks.sort((a, b) => {
+      const timestampA = new Date(a.updatedAt).getTime();
+      const timestampB = new Date(b.updatedAt).getTime();
+      return timestampB - timestampA;
+    });
+
+    setSortedTasks(sortedTasks);
+  }, [tasks]);
+
   return (
     <ul className={css.columnTasksList}>
-      {tasks.map((task, index) => (
+      {sortedTasks.map((task, index) => (
         <li key={task._id}>
           <TaskColumnCard
             task={task}
             user={user}
             openPopup={openPopup}
             handleTask={handleTask}
-            isLast={index === tasks.length - 1} // Передаем isLast как пропс
+            isLast={index === tasks.length - 1}
           />
         </li>
       ))}
